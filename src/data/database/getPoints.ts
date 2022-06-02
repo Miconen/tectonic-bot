8e2ddd5be7d064478415c435ce81e611237e3cf0
Miconen <micropizza.mc@gmail.com>
2022-06-02 16:29:46 +0300

    Database integration for querying points

---
+++ src/data/database/getPoints.ts
@@ -0,0 +1,27 @@
+import pool from './pooling.js';
+
+const SELECT_QUERY = `SELECT points FROM users WHERE (guild_id=$1 AND user_id=$2)`;
+
+const getPoints = async (
+	guild_id: string,
+	user_id: string,
+	callback: (arg0: number) => void
+) => {
+	let result = 0;
+	const connection = await pool.connect();
+	connection
+		.query(SELECT_QUERY, [guild_id, user_id])
+		.then((res) => {
+			if (res.rows[0].points) result = res.rows[0].points;
+
+			callback(result);
+		})
+		.catch((err) => {
+			console.error(err);
+		})
+		.finally(() => {
+			connection.release();
+		});
+};
+
+export default getPoints;
