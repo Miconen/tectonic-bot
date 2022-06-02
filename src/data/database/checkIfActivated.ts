a96b40ea62f707be90a640564ab4fa0d033c2c44
Miconen <micropizza.mc@gmail.com>
2022-06-02 15:11:23 +0300

    Fix activated users db check

--- src/data/database/checkIfActivated.ts
+++ src/data/database/checkIfActivated.ts
@@ -1,8 +1,8 @@
 import pool from './pooling.js';
 
-const CHECK_QUERY = `SELECT COUNT(1) FROM users WHERE (guild_id='979445890064470036' AND user_id='136856906139566081')`;
+const CHECK_QUERY = `SELECT COUNT(1) FROM users WHERE (guild_id=$1 AND user_id=$2)`;
 
-const removeUser = async (
+const checkIfActivated = async (
 	guild_id: string,
 	user_id: string,
 	callback: (arg0: boolean) => void
@@ -10,12 +10,10 @@ const removeUser = async (
 	let result = false;
 	const connection = await pool.connect();
 	connection
-		.query(CHECK_QUERY)
+		.query(CHECK_QUERY, [guild_id, user_id])
 		.then((res) => {
-			console.log(res.rows[0].count);
-
-			if (res.rows[0].count) result = true;
-			if (!res.rows[0].count) result = false;
+			if (res.rows[0].count == 1) result = true;
+			if (res.rows[0].count == 0) result = false;
 
 			callback(result);
 		})
@@ -27,4 +25,4 @@ const removeUser = async (
 		});
 };
 
-export default removeUser;
+export default checkIfActivated;
