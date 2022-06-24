e4c8c39645e00617e4a7b166661b758d7c426e1d
Miconen <micropizza.mc@gmail.com>
2022-06-24 17:48:07 +0300

    Command for rsns

--- src/commands/rsn.ts
+++ src/commands/rsn.ts
@@ -1,18 +1,60 @@
 import { Discord, Slash, SlashOption, SlashGroup, SlashChoice } from 'discordx';
 import { CommandInteraction, User } from 'discord.js';
+import IsAdmin from '../utility/isAdmin.js';
+import setRsn from '../data/database/setRsn.js';
 
 @Discord()
 @SlashGroup({ name: 'rsn', description: 'Runescape name related commands' })
 @SlashGroup('rsn')
 class RSN {
 	@Slash('set')
-	lookup(
+	give(
 		@SlashOption('username')
-		username: string,
+		channel: User,
+
+		@SlashOption('rsname', { description: 'Users RSN' })
+		rsname: string,
+
+		@SlashChoice({ name: 'Hardcore Group Ironman', value: 'HCGIM' })
+		@SlashChoice({ name: 'Group Ironman', value: 'GIM' })
+		@SlashChoice({ name: 'Ultimate Ironman', value: 'UIM' })
+		@SlashChoice({ name: 'Hardcore Ironman', value: 'HCIM' })
+		@SlashChoice({ name: 'Ironman', value: 'IM' })
+		@SlashChoice({ name: 'Main', value: 'MAIN' })
+		@SlashOption('type', { description: 'Users account/ironman type'})
+		type: string,
+
 		interaction: CommandInteraction
 	) {
-		// TODO: SANITISE THE USERNAME INPUT TO AVOID SQL INJECTION
-		interaction.reply(`${username}`);
+		if (!IsAdmin(Number(interaction.member?.permissions))) return;
+
+		let result = setRsn(
+			interaction.guildId!,
+			// @ts-ignore
+			channel.user.id,
+			rsname,
+			type
+		);
+
+		let response = 'Error settings rsn';
+		result
+			.then((res: any) => {
+				console.log(res);
+				if (Number.isInteger(res)) {
+					// @ts-ignore
+					response = `✔️ ${channel.user} was granted ${points} points by ${interaction.member} and now has a total of ${res} points.`;
+				}
+				if (res == false) {
+					// @ts-ignore
+					response = `❌ ${channel.user} Is not an activated user.`;
+				}
+			})
+			.catch((err: any) => {
+				console.log(err);
+			})
+			.finally(() => {
+				interaction.reply(response);
+			});
 	}
 	@Slash('setiron')
 	setiron(
