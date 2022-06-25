b4438f23e69ef1bf623e208bdaab844c56fa5264
Miconen <micropizza.mc@gmail.com>
2022-06-25 13:25:28 +0300

    Help command basics

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,14 +1,19 @@
 import { Discord, Slash, SlashOption, SlashGroup } from 'discordx';
 import { CommandInteraction, User } from 'discord.js'
+import pointsHandler, { PointRewardsMap } from '../data/pointHandling.js';
 
 @Discord()
 @SlashGroup({ name: 'help', description: 'Commands to help you use commands' })
-@SlashGroup('points')
+@SlashGroup('help')
 class Help {
 	@Slash('bump')
 	lookup(
 		interaction: CommandInteraction
 	) {
-        interaction.reply('Points for activity: ');
+		let points = pointsHandler(PointRewardsMap.get('forum_bump'), interaction.guild!.id)
+		
+		points.then((res: any) => {
+			interaction.reply(`You can gain points for bumping our forum post here: (insert link).\nPoints reward: ${res}`);
+		});
     }
 }
