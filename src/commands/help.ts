e2d764b34639e39d8f5b2afb310fed65c96e6a2b
Miconen <micropizza.mc@gmail.com>
2022-10-19 18:50:42 +0300

    Fixing old commands

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,83 +1,82 @@
-import { Discord, Slash, SlashGroup } from "discordx";
-import { CommandInteraction } from "discord.js";
-import pointsHandler, { PointRewardsMap } from "../data/pointHandling.js";
+import { Discord, Slash, SlashGroup } from 'discordx';
+import { CommandInteraction } from 'discord.js';
+import pointsHandler, { PointRewardsMap } from '../data/pointHandling.js';
 
 @Discord()
-@SlashGroup({ name: "help", description: "Commands to help you use commands" })
-@SlashGroup("help")
+@SlashGroup({ name: 'help', description: 'Commands to help you use commands' })
+@SlashGroup('help')
 class Help {
-    @Slash({
-        name: "bump",
-        description: "Information about bumping the forum post",
-    })
-    bump(interaction: CommandInteraction) {
-        let points = pointsHandler(
-            PointRewardsMap.get("forum_bump"),
-            interaction.guild!.id,
-        );
+	@Slash({
+		name: 'bump',
+		description: 'Information about bumping the forum post',
+	})
+	bump(interaction: CommandInteraction) {
+		let points = pointsHandler(
+			PointRewardsMap.get('forum_bump'),
+			interaction.guild!.id
+		);
 
-        points.then((res: any) => {
-            interaction.reply(
-                `You can gain points for bumping our forum post here: (insert link).\nPoint reward: ${res}`,
-            );
-        });
-    }
-    @Slash({ name: "split", description: "Information about splitting" })
-    async split(interaction: CommandInteraction) {
-        let points_low = await pointsHandler(
-            PointRewardsMap.get("split_low"),
-            interaction.guild!.id,
-        );
-        let points_medium = await pointsHandler(
-            PointRewardsMap.get("split_medium"),
-            interaction.guild!.id,
-        );
-        let points_high = await pointsHandler(
-            PointRewardsMap.get("split_high"),
-            interaction.guild!.id,
-        );
+		points.then((res: any) => {
+			interaction.reply(
+				`You can gain points for bumping our forum post here: (insert link).\nPoint reward: ${res}`
+			);
+		});
+	}
+	@Slash({ name: 'split', description: 'Information about splitting' })
+	async split(interaction: CommandInteraction) {
+		let points_low = await pointsHandler(
+			PointRewardsMap.get('split_low'),
+			interaction.guild!.id
+		);
+		let points_medium = await pointsHandler(
+			PointRewardsMap.get('split_medium'),
+			interaction.guild!.id
+		);
+		let points_high = await pointsHandler(
+			PointRewardsMap.get('split_high'),
+			interaction.guild!.id
+		);
 
-        interaction.reply(
-            `Gain points for recieving a drop and splitting with your clan mates, screenshot of loot and teammates names required as proof.\nRequires user to be an activated user\nPoint rewards: ${points_low}, ${points_medium} & ${points_high}`,
-        );
-    }
-    @Slash({
-        name: "activate",
-        description: "Information about activating clan members",
-    })
-    activate(interaction: CommandInteraction) {
-        interaction.reply(
-            `Admin only command, used to activate new member of the community. This command handles all automation for new users and allows them to gain points/ranks. This should be used on all new users that are a part of the clan.`,
-        );
-    }
-    @Slash({ name: "github", description: "Information about the bot" })
-    github(interaction: CommandInteraction) {
-        interaction.reply(
-            "Link to Tectonic Bot repository: https://github.com/Miconen/tectonic-bot if you want to contribute @ Comfy hug",
-        );
-    }
+		interaction.reply(
+			`Gain points for recieving a drop and splitting with your clan mates, screenshot of loot and teammates names required as proof.\nRequires user to be an activated user\nPoint rewards: ${points_low}, ${points_medium} & ${points_high}`
+		);
+	}
+	@Slash({
+		name: 'activate',
+		description: 'Information about activating clan members',
+	})
+	activate(interaction: CommandInteraction) {
+		interaction.reply(
+			`Admin only command, used to activate new member of the community. This command handles all automation for new users and allows them to gain points/ranks. This should be used on all new users that are a part of the clan.`
+		);
+	}
+	@Slash({ name: 'github', description: 'Information about the bot' })
+	github(interaction: CommandInteraction) {
+		interaction.reply(
+			'Link to Tectonic Bot repository: https://github.com/Miconen/tectonic-bot if you want to contribute @ Comfy hug'
+		);
+	}
 }
 
 @Discord()
 @SlashGroup({
-    name: "rsn",
-    description: "Help for the rsn related commands",
-    root: "help",
+	name: 'rsn',
+	description: 'Help for the rsn related commands',
+	root: 'help',
 })
-@SlashGroup("rsn", "help")
+@SlashGroup('rsn', 'help')
 class HelpRsn {
-    @Slash({ name: "set", description: "Information about setting your rsn" })
-    set(interaction: CommandInteraction) {
-        interaction.reply(
-            "Temporarily admin only command, bind your osrs account(s) to your discord user.",
-        );
-    }
-    @Slash({
-        name: "lookup",
-        description: "Information about other clan members osrs accounts",
-    })
-    lookup(interaction: CommandInteraction) {
-        interaction.reply("Lookup discord users osrs account(s)");
-    }
+	@Slash({ name: 'set', description: 'Information about setting your rsn' })
+	set(interaction: CommandInteraction) {
+		interaction.reply(
+			'Temporarily admin only command, bind your osrs account(s) to your discord user.'
+		);
+	}
+	@Slash({
+		name: 'lookup',
+		description: 'Information about other clan members osrs accounts',
+	})
+	lookup(interaction: CommandInteraction) {
+		interaction.reply('Lookup discord users osrs account(s)');
+	}
 }
-
--- src/commands/learner.ts
+++ src/commands/learner.ts
@@ -1,98 +1,100 @@
 import {
-    ApplicationCommandOptionType,
-    CommandInteraction,
-    GuildMember,
-} from "discord.js";
-import { Discord, Slash, SlashGroup, SlashOption } from "discordx";
-import updateUserPoints from "../data/database/updateUserPoints.js";
-import pointsHandler, { PointRewardsMap } from "../data/pointHandling.js";
-import { rankUpHandler } from "../data/roleHandling.js";
-import IsAdmin from "../utility/isAdmin.js";
+	ApplicationCommandOptionType,
+	CommandInteraction,
+	GuildMember,
+} from 'discord.js';
+import { Discord, Slash, SlashGroup, SlashOption } from 'discordx';
+import updateUserPoints from '../data/database/updateUserPoints.js';
+import pointsHandler, { PointRewardsMap } from '../data/pointHandling.js';
+import { rankUpHandler } from '../data/roleHandling.js';
+import IsAdmin from '../utility/isAdmin.js';
 
 @Discord()
-@SlashGroup({ name: "learner", description: "Learner specific point commands" })
+@SlashGroup({ name: 'learner', description: 'Learner specific point commands' })
+@SlashGroup('learner')
 class Learner {
-    @Slash({ name: "half", description: "Halved learner points" })
-    async half(
-        @SlashOption({
-            name: "username",
-            description: "Users discord profile",
-            type: ApplicationCommandOptionType.User,
-        })
-        channel: GuildMember,
-        interaction: CommandInteraction,
-    ) {
-        if (!IsAdmin(Number(interaction.member?.permissions))) return;
-        let points = await pointsHandler(
-            PointRewardsMap.get("learner_half"),
-            interaction.guild!.id,
-        );
-        let result = updateUserPoints(
-            interaction.guild!.id,
-            // @ts-ignore
-            channel.user.id,
-            points!,
-        );
-        let response = "Error giving points";
-        result
-            .then((res) => {
-                if (Number.isInteger(res)) {
-                    // @ts-ignore
-                    response = `✔️ ${channel.user} was granted ${points} points by ${interaction.member} and now has a total of ${res} points.`;
-                    rankUpHandler(interaction, channel, res - points, res);
-                }
-                if (res == false) {
-                    // @ts-ignore
-                    response = `❌ ${channel.user} Is not an activated user.`;
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-            .finally(() => {
-                interaction.reply(response);
-            });
-    }
-    @Slash({ name: "full", description: "Full learner points" })
-    async full(
-        @SlashOption({
-            name: "username",
-            description: "Users discord profile",
-            type: ApplicationCommandOptionType.User,
-        })
-        channel: GuildMember,
-        interaction: CommandInteraction,
-    ) {
-        if (!IsAdmin(Number(interaction.member?.permissions))) return;
-        let points = await pointsHandler(
-            PointRewardsMap.get("learner_full"),
-            interaction.guild!.id,
-        );
-        let result = updateUserPoints(
-            interaction.guild!.id,
-            // @ts-ignore
-            channel.user.id,
-            points!,
-        );
-        let response = "Error giving points";
-        result
-            .then((res) => {
-                if (Number.isInteger(res)) {
-                    // @ts-ignore
-                    response = `✔️ ${channel.user} was granted ${points} points by ${interaction.member} and now has a total of ${res} points.`;
-                    rankUpHandler(interaction, channel, res - points, res);
-                }
-                if (res == false) {
-                    // @ts-ignore
-                    response = `❌ ${channel.user} Is not an activated user.`;
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-            .finally(() => {
-                interaction.reply(response);
-            });
-    }
+	@Slash({ name: 'half', description: 'Halved learner points' })
+	async half(
+		@SlashOption({
+			name: 'username',
+			description: 'Users discord profile',
+			required: true,
+			type: ApplicationCommandOptionType.User,
+		})
+		channel: GuildMember,
+		interaction: CommandInteraction
+	) {
+		if (!IsAdmin(Number(interaction.member?.permissions))) return;
+		let points = await pointsHandler(
+			PointRewardsMap.get('learner_half'),
+			interaction.guild!.id
+		);
+		let result = updateUserPoints(
+			interaction.guild!.id,
+			// @ts-ignore
+			channel.user.id,
+			points!
+		);
+		let response = 'Error giving points';
+		result
+			.then((res) => {
+				if (Number.isInteger(res)) {
+					// @ts-ignore
+					response = `✔️ ${channel.user} was granted ${points} points by ${interaction.member} and now has a total of ${res} points.`;
+					rankUpHandler(interaction, channel, res - points, res);
+				}
+				if (res == false) {
+					// @ts-ignore
+					response = `❌ ${channel.user} Is not an activated user.`;
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				interaction.reply(response);
+			});
+	}
+	@Slash({ name: 'full', description: 'Full learner points' })
+	async full(
+		@SlashOption({
+			name: 'username',
+			description: 'Users discord profile',
+			required: true,
+			type: ApplicationCommandOptionType.User,
+		})
+		channel: GuildMember,
+		interaction: CommandInteraction
+	) {
+		if (!IsAdmin(Number(interaction.member?.permissions))) return;
+		let points = await pointsHandler(
+			PointRewardsMap.get('learner_full'),
+			interaction.guild!.id
+		);
+		let result = updateUserPoints(
+			interaction.guild!.id,
+			// @ts-ignore
+			channel.user.id,
+			points!
+		);
+		let response = 'Error giving points';
+		result
+			.then((res) => {
+				if (Number.isInteger(res)) {
+					// @ts-ignore
+					response = `✔️ ${channel.user} was granted ${points} points by ${interaction.member} and now has a total of ${res} points.`;
+					rankUpHandler(interaction, channel, res - points, res);
+				}
+				if (res == false) {
+					// @ts-ignore
+					response = `❌ ${channel.user} Is not an activated user.`;
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				interaction.reply(response);
+			});
+	}
 }
-
