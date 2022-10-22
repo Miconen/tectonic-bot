adead8c09c0e314f70b9264fbb187aecb662f17e
Miconen <micropizza.mc@gmail.com>
2022-10-22 23:20:08 +0300

    Small refactoring

--- src/utility/isAdmin.ts
+++ src/utility/isAdmin.ts
@@ -4,10 +4,7 @@ function IsAdmin(bitfield: number) {
 	 */
 
 	//binary operations spaggeti, admin flag is bit 40....
-	const admin_flag =
-		(BigInt(Number(bitfield?.valueOf())) & (1n << 40n)) >> 40n;
-
-	return admin_flag;
+	return (BigInt(Number(bitfield?.valueOf())) & (1n << 40n)) >> 40n;
 }
 
 export default IsAdmin;
