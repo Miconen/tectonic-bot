fa6e2bb5df48b96f897133e37f01a3a5ff96f037
Miconen <micropizza.mc@gmail.com>
2022-10-22 05:58:36 +0300

    Update pooling to use env variables

--- src/data/database/pooling.ts
+++ src/data/database/pooling.ts
@@ -1,10 +1,12 @@
-import mysql from 'mysql';
+import "dotenv/config";
+import mysql from "mysql";
 const pool = mysql.createPool({
-	connectionLimit: 10,
-	host: 'db',
-	user: 'root',
-	password: 'root',
-	database: 'tectonic',
+    connectionLimit: 10,
+    host: process.env.MYSQLHOST ?? "localhost",
+    port: Number.parseInt(process.env.MYSQLPORT ?? "3306"),
+    user: process.env.MYSQLUSER ?? "root",
+    password: process.env.MYSQLPASSWORD ?? "root",
+    database: process.env.MYSQLDATABASE ?? "tectonic",
 });
 
 export default pool;
