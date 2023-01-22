b52cbdc6d0f840bef8c420825e0fc03e2f12dfc2
Miconen <micropizza.mc@gmail.com>
2023-01-22 06:08:39 +0200

    Renamed function name to match intended functionality

--- src/commands/moderation/func/giveHelper.ts
+++ src/commands/moderation/func/giveHelper.ts
@@ -2,11 +2,11 @@ import {CommandInteraction, GuildMember} from "discord.js";
 import IsAdmin from "../../../utility/isAdmin.js";
 import * as pointUtils from "../../../utility/pointUtils/index.js";
 
-const multiplierHelper = async (user: GuildMember, addedPoints: number, interaction: CommandInteraction) => {
+const giveHelper = async (user: GuildMember, addedPoints: number, interaction: CommandInteraction) => {
     if (!IsAdmin(Number(interaction.member?.permissions))) return;
 
     // Handle giving of points, returns a string to be sent as a message.
     await pointUtils.givePoints(addedPoints, user, interaction);
 }
 
-export default multiplierHelper;
+export default giveHelper;
