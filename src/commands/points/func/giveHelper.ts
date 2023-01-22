daf28603ba16df149797f319df78902f9e493d2c
Miconen <micropizza.mc@gmail.com>
2023-01-22 06:15:13 +0200

    Separate functionality from command definition for the points command

---
+++ src/commands/points/func/giveHelper.ts
@@ -0,0 +1,25 @@
+import {CommandInteraction, GuildMember} from "discord.js";
+import { getPoints } from '../../../database/getUser.js';
+import * as rankUtils from "../../../utility/rankUtils/index.js";
+
+const pointsHelper = async (user: GuildMember | null, interaction: CommandInteraction) => {
+    let targetUser = user?.user?.id ?? interaction.user.id ?? "0";
+    let targetUserName = user?.displayName ?? (interaction.member as GuildMember).displayName ?? "???";
+    let points = await getPoints(interaction.guildId!, targetUser);
+
+    let response: string;
+    if (points || points === 0) {
+        let nextRankUntil = rankUtils.pointsToNextRank(points);
+        let nextRankIcon = rankUtils.rankIcon.get(rankUtils.getRankByPoints(points + nextRankUntil));
+
+        response = `${rankUtils.rankIcon.get(rankUtils.getRankByPoints(points))} **${targetUserName}** has: ${points} points`;
+        if (rankUtils.getRankByPoints(points) != "zenyte") response += `\n${nextRankIcon} Points to next level: ${nextRankUntil}`;
+    }
+    else {
+        response = `❌ ${targetUserName} is not activated.`;
+    }
+
+    await interaction.reply(response);
+}
+
+export default pointsHelper;
--- src/commands/points/points.ts
+++ src/commands/points/points.ts
@@ -4,8 +4,7 @@ import {
 	CommandInteraction,
 	GuildMember,
 } from 'discord.js';
-import { getPoints } from '../../database/getUser.js';
-import * as rankUtils from "../../utility/rankUtils/index.js";
+import giveHelper from "./func/giveHelper";
 
 @Discord()
 class Points {
@@ -24,22 +23,6 @@ class Points {
 		user: GuildMember | null,
 		interaction: CommandInteraction
 	) {
-		let targetUser = user?.user?.id ?? interaction.user.id ?? "0";
-		let targetUserName = user?.displayName ?? (interaction.member as GuildMember).displayName ?? "???";
-		let points = await getPoints(interaction.guildId!, targetUser);
-
-		let response: string;
-		if (points || points === 0) {
-			let nextRankUntil = rankUtils.pointsToNextRank(points);
-			let nextRankIcon = rankUtils.rankIcon.get(rankUtils.getRankByPoints(points + nextRankUntil));
-
-			response = `${rankUtils.rankIcon.get(rankUtils.getRankByPoints(points))} **${targetUserName}** has: ${points} points`;
-			if (rankUtils.getRankByPoints(points) != "zenyte") response += `\n${nextRankIcon} Points to next level: ${nextRankUntil}`;
-		}
-		else {
-			response = `❌ ${targetUserName} is not activated.`;
-		}
-
-		await interaction.reply(response);
+		return giveHelper(user, interaction);
 	}
 }
