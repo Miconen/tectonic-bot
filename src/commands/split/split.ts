917bbabc4a5efe38cfeb77f0223b70bc3c8e0003
Miconen <micropizza.mc@gmail.com>
2023-01-22 06:55:48 +0200

    Split functionality and declaration of split commands

--- src/commands/split/split.ts
+++ src/commands/split/split.ts
@@ -1,11 +1,7 @@
 import {
     ButtonInteraction,
     CommandInteraction,
-    GuildMember,
-    ActionRowBuilder,
-    ButtonBuilder,
     ButtonStyle,
-    MessageActionRowComponentBuilder,
     ApplicationCommandOptionType,
 } from "discord.js";
 import {
@@ -15,39 +11,17 @@ import {
     SlashChoice,
     SlashOption,
 } from "discordx";
-import IsAdmin from "../../utility/isAdmin.js";
 import * as pointUtils from "../../utility/pointUtils/index.js";
-import * as rankUtils from "../../utility/rankUtils/index.js";
-import updateUserPoints from "../../database/updateUserPoints.js";
-import capitalizeFirstLetter from "../../utility/capitalizeFirstLetter.js";
+import splitHelper from "./func/splitHelper.js";
+import acceptHelper from "./func/acceptHelper.js";
+import {InteractionCache} from "./func/InteractionCache.js";
+import denyHelper from "./func/denyHelper.js";
 
-const interactionMap = new Map<string, CommandInteraction>();
-const interactionState = new Map<string, boolean>();
-const pointsMap = new Map<string, number>();
-
-const getInteractionId = (interaction: ButtonInteraction) => {
-    if (!interaction.message.interaction?.id)
-        console.log("ERROR: Interaction ID defaulted to 0");
-    return interaction.message.interaction?.id ?? "0";
-};
-
-const isValid = async (interaction: ButtonInteraction) => {
-    let interactionId = getInteractionId(interaction);
-    // return if not admin
-    if (!IsAdmin(Number(interaction.member?.permissions))) return false;
-    // If command has not been stored in memory, don't run.
-    // Idea is not to handle commands that haven't been stored since restart.
-    if (!interactionState.has(interactionId)) {
-        await interaction.reply("❌ Point request expired...");
-        return false;
-    }
-    // If command has been run once, don't run again. Returns true if ran once.
-    if (interactionState.get(interactionId)) {
-        await interaction.reply("❌ Points already handled");
-        return false;
-    }
-    return true;
-};
+let state: InteractionCache = {
+    interactionMap: new Map<string, CommandInteraction>(),
+    interactionState: new Map<string, boolean>(),
+    pointsMap: new Map<string, number>(),
+}
 
 @Discord()
 class split {
@@ -74,130 +48,18 @@ class split {
         value: number,
         interaction: CommandInteraction,
     ) {
-        await interaction.deferReply();
-        value = await pointUtils.pointsHandler(value, interaction.guild!.id);
-
-        // Create the button, giving it the id: "approve-btn"
-        const approveButton = new ButtonBuilder()
-            .setLabel("Approve")
-            .setStyle(ButtonStyle.Primary)
-            .setCustomId("approve-btn");
-
-        // Create a button, giving it the id: "deny-btn"
-        const denyButton = new ButtonBuilder()
-            .setLabel("Deny")
-            .setStyle(ButtonStyle.Danger)
-            .setCustomId("deny-btn");
-
-        // Create a MessageActionRow and add the button to that row.
-        const row =
-            new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-                approveButton,
-                denyButton,
-            );
-
-        const msg = `**${(interaction.member as GuildMember).displayName
-            }** has submitted a request for ${value} points. Please wait for admin approval and make sure you have posted a screenshot of your drop as proof.`;
-        await interaction.editReply({
-            content: msg,
-            components: [row],
-        });
-
-        interactionMap.set(interaction.id, interaction);
-        interactionState.set(interaction.id, false);
-        pointsMap.set(interaction.id, value);
+        await splitHelper(value, interaction, state);
     }
 
     // register a handler for the button with id: "approve-btn"
     @ButtonComponent({ id: "approve-btn" })
     async approveButton(interaction: ButtonInteraction) {
-        if (!(await isValid(interaction))) return;
-
-        let interactionId = getInteractionId(interaction);
-
-        let receivingInteraction = interactionMap.get(
-            interactionId,
-        ) as CommandInteraction;
-        let grantingUser = interaction.member as GuildMember;
-        let receivingUser = receivingInteraction.member as GuildMember;
-        if (!receivingUser) {
-            await interaction.reply("Error parsing interaction map");
-            console.log("ERROR: Couldn't get interaction from interactionMap");
-            return;
-        }
-        let grantingUserName = grantingUser.displayName;
-        let receivingUserName = receivingUser.displayName;
-
-        let addedPoints = pointsMap.get(interactionId) ?? 0;
-        let totalPoints = await updateUserPoints(
-            interaction.guild!.id,
-            interaction.message.interaction!.user.id,
-            addedPoints,
-        );
-
-        let response: string;
-        // Check for 0 since it evaluates to false otherwise
-        if (totalPoints || totalPoints === 0) {
-            response = `✔ **${receivingUserName}** was granted ${addedPoints} points by **${grantingUserName}** and now has a total of ${totalPoints} points.`;
-            let newRank = await rankUtils.rankUpHandler(
-                { interaction: receivingInteraction, target: receivingUser, oldPoints: totalPoints - addedPoints, newPoints: totalPoints },
-            );
-
-            // Concatenate level up message to response if user leveled up
-            if (newRank) {
-                let newRankIcon = rankUtils.rankIcon.get(newRank);
-                response += `\n**${receivingUser}** ranked up to ${newRankIcon} ${capitalizeFirstLetter(
-                    newRank,
-                )}!`;
-            }
-
-            // Remove buttons on successful button press
-            await receivingInteraction.editReply({
-                components: [],
-            });
-        } else if (totalPoints === false) {
-            response = `❌ **${receivingUser}** is not an activated user.`;
-        } else {
-            response = "Error giving points";
-        }
-
-        // Free up memory on point approval
-        interactionMap.delete(interactionId);
-        interactionState.delete(interactionId);
-        pointsMap.delete(interactionId);
-
-        await interaction.reply(response);
+        return acceptHelper(interaction, state);
     }
 
     // register a handler for the button with id: "deny-btn"
     @ButtonComponent({ id: "deny-btn" })
     async denyButton(interaction: ButtonInteraction) {
-        if (!(await isValid(interaction))) return;
-
-        let interactionId = getInteractionId(interaction);
-        let receivingInteraction = interactionMap.get(
-            interactionId,
-        ) as CommandInteraction;
-        let receivingUser = receivingInteraction.member as GuildMember;
-        if (!receivingUser) {
-            await interaction.reply("Error parsing interaction map");
-            console.log("ERROR: Couldn't get interaction from interactionMap");
-            return;
-        }
-        let receivingUserName = receivingUser.displayName;
-
-        // Remove buttons on successful button press
-        await receivingInteraction.editReply({
-            components: [],
-        });
-
-        // Free up memory on point denial
-        interactionMap.delete(interactionId);
-        interactionState.delete(interactionId);
-        pointsMap.delete(interactionId);
-
-        await interaction.reply(
-            `❌ **${receivingUserName}** point request was denied.`,
-        );
+        return denyHelper(interaction, state);
     }
 }
