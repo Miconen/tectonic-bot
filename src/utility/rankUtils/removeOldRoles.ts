2eed17b10846ddee49058a23f50df81acfacc814
Miconen <micropizza.mc@gmail.com>
2023-03-01 15:51:54 +0200

    Fix old roles not getting removed

--- src/utility/rankUtils/removeOldRoles.ts
+++ src/utility/rankUtils/removeOldRoles.ts
@@ -1,11 +1,20 @@
 import { GuildMember } from "discord.js";
-import { roleValues } from "./rankData.js";
+import { roleIds } from "./rankData.js";
 
 const removeOldRoles = async (target: GuildMember) => {
-    const oldRoles = target.roles.cache.filter(role => roleValues.has(Number(role.id)));
+    const oldRoles = target.roles.cache.filter(role => getByValue(roleIds, role.id));
     if (oldRoles.size === 0)
         return;
     await target.roles.remove(oldRoles);
 };
 
 export { removeOldRoles };
+
+// Get role id from the roleIds map using a value instead of a key
+function getByValue(map: Map<string, string>, searchValue: string) {
+    for (let [key, value] of map.entries()) {
+        if (value === searchValue)
+            return key;
+    }
+    return;
+}
