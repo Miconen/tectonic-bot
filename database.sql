b52f4e2cf398234f40e63359f957e420e6d11d0f
Dayrren <isafor-0@student.ltu.se>
2023-03-09 20:43:45 +0100

    Database refactoring

--- database.sql
+++ database.sql
@@ -1,60 +1,25 @@
-CREATE TABLE `users`
-(
- `id`       integer NOT NULL AUTO_INCREMENT,
- `guild_id` varchar(32) NOT NULL ,
- `user_id`  varchar(32) NOT NULL ,
- `points`   integer NOT NULL DEFAULT 0 ,
+CREATE TABLE `guilds` (
+ `id` int NOT NULL AUTO_INCREMENT,
+ `guild_id` varchar(32) NOT NULL,
+ `multiplier` int NOT NULL DEFAULT '1',
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `guild_id` (`guild_id`)
+)
 
-PRIMARY KEY (`id`)
-);
+CREATE TABLE `users` (
+ `user_id` varchar(32) NOT NULL,
+ `guild_id` varchar(32) NOT NULL,
+ `points` int NOT NULL DEFAULT '0',
+ PRIMARY KEY (`user_id`,`guild_id`),
+ KEY `guild_id` (`guild_id`),
+ CONSTRAINT `users_ibfk_1` FOREIGN KEY (`guild_id`) REFERENCES `guilds` (`guild_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
 
-
-CREATE TABLE `splits`
-(
- `id`             integer NOT NULL AUTO_INCREMENT,
- `user`           integer NOT NULL ,
- `split_amount`   integer NOT NULL ,
- `split_item`     varchar(50) NOT NULL ,
- `points_awarded` integer NOT NULL ,
-
-PRIMARY KEY (`id`),
-KEY `FK_21` (`user`),
-CONSTRAINT `FK_19` FOREIGN KEY `FK_21` (`user`) REFERENCES `users` (`id`)
-);
-
-
-CREATE TABLE `transactions`
-(
- `id`             integer NOT NULL AUTO_INCREMENT,
- `user`           integer NOT NULL ,
- `points_awarded` integer NOT NULL ,
- `points_reason`  varchar(50) NOT NULL ,
-
-PRIMARY KEY (`id`),
-KEY `FK_28` (`user`),
-CONSTRAINT `FK_26` FOREIGN KEY `FK_28` (`user`) REFERENCES `users` (`id`)
-);
-
-CREATE TABLE `rsn`
-(
- `id`      integer NOT NULL AUTO_INCREMENT ,
- `user`    integer NOT NULL ,
- `rsn`     varchar(32) NOT NULL ,
- `type` varchar(8) NOT NULL ,
-
-UNIQUE (rsn), 
-
-PRIMARY KEY (`id`),
-KEY `FK_29` (`user`),
-CONSTRAINT `FK_27` FOREIGN KEY `FK_29` (`user`) REFERENCES `users` (`id`)
-);
-
-CREATE TABLE `guilds`
-(
- `id`       integer NOT NULL AUTO_INCREMENT,
- `guild_id` varchar(32) NOT NULL ,
- `multiplier`   integer NOT NULL DEFAULT 1 ,
-
-UNIQUE (guild_id),
-PRIMARY KEY (`id`)
+CREATE TABLE `rsn` (
+ `discord_id` varchar(32) NOT NULL,
+ `wom_id` varchar(32) NOT NULL,
+ `rsn` varchar(32) NOT NULL,
+ PRIMARY KEY (`discord_id`,`wom_id`),
+ UNIQUE KEY `rsn` (`rsn`),
+ CONSTRAINT `rsn_ibfk_1` FOREIGN KEY (`discord_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 )
