47f3fd67535f7861bc90c215355c8bcbbeb3082a
Miconen <micropizza.mc@gmail.com>
2023-06-02 15:26:33 +0300

    Update IsAdmin

--- src/utility/isAdmin.ts
+++ src/utility/isAdmin.ts
@@ -1,6 +1,20 @@
-function IsAdmin(input: number) {
-	let bitfield = BigInt(input);
-	return (bitfield & (1n << 40n)) >> 40n;
+import { ButtonInteraction, CommandInteraction } from "discord.js";
+import { GuardFunction } from "discordx";
+
+function hasPermissions(input: number) {
+    let bitfield = BigInt(input);
+    return !!((bitfield & (1n << 40n)) >> 40n);
+}
+
+export const IsAdmin: GuardFunction<ButtonInteraction | CommandInteraction> = async (arg, _, next) => {
+    const argObj = arg instanceof Array ? arg[0] : arg;
+    const permissions = argObj.member.permissions;
+
+    console.log(permissions);
+    console.log(hasPermissions(permissions));
+    if (hasPermissions(permissions)) {
+        await next();
+    }
 }
 
 export default IsAdmin;
