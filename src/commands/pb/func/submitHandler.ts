1e8ff6640fdca9b6d2c2cd3c6afa45b4a801844e
Miconen <micropizza.mc@gmail.com>
2023-07-01 21:28:51 +0300

    Update position of interaction parameter preventing it from being overwritten

--- src/commands/pb/func/submitHandler.ts
+++ src/commands/pb/func/submitHandler.ts
@@ -1,18 +1,31 @@
 import { CommandInteraction } from "discord.js";
 import addTime from "./addTime.js";
 import TimeConverter from "./TimeConverter.js";
-import timeToTicks from "./timeToTicks.js";
 import updateEmbed from "./updateEmbed.js";
 import updatePb from "./updatePb.js";
 
 async function submitHandler(boss: string, time: string, team: (string | undefined)[], interaction: CommandInteraction) {
-    const guildId = interaction.guildId!;
+    console.log(`Submitting pb: ${boss} ${time}`);
+    const guildId = interaction?.guildId;
+    if (!guildId) {
+        return console.log("↳ Failed getting guildId")
+    };
     const ticks = TimeConverter.timeToTicks(time);
-    if (!ticks) return;
+    if (!ticks) {
+        return console.log("↳ Failed parsing ticks from time")
+    };
     const addedTime = await addTime(ticks, boss, team, guildId);
-    if (!addedTime) return;
+    if (!addedTime) {
+        return console.log("↳ Failed adding time")
+    } else {
+        console.log("↳ Time added")
+    };
     const updated = await updatePb(addedTime.time, addedTime.run_id, boss, guildId);
-    if (!updated) return;
+    if (!updated) {
+        return console.log("↳ Not a new pb")
+    } else {
+        console.log("↳ New pb")
+    };
     await updateEmbed(boss, guildId, interaction);
 }
 
