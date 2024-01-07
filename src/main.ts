b4a511aab42014ab51bbca1324e6a53ab0e3e906
isak <isak.forsgren@gmail.com>
2024-01-07 05:22:50 +0100

    Added event to auto remove members

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ import { container } from "tsyringe"
 
 // TEMPORARY FIX TO THIS: https://github.com/oceanroleplay/discord.ts/issues/840
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-;(BigInt.prototype as any).toJSON = function () {
+;import events from "events"
+(BigInt.prototype as any).toJSON = function () {
     return this.toString()
 }
 
@@ -28,6 +29,7 @@ export const bot = new Client({
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.GuildMessageReactions,
+        IntentsBitField.Flags.GuildPresences, // ADD THIS
     ],
 
     // Debug logs are disabled in silent mode
@@ -46,6 +48,7 @@ bot.once("ready", async () => {
     // Synchronize applications commands with Discord
     await bot.initApplicationCommands()
 
+
     // To clear all guild commands, uncomment this line,
     // This is useful when moving from guild commands to global commands
     // It must only be executed once
@@ -63,6 +66,7 @@ bot.on("messageCreate", (message: Message) => {
     bot.executeCommand(message)
 })
 
+
 async function run() {
     container.registerSingleton("Database", Database)
     container.registerSingleton("RankService", RankService)
@@ -70,8 +74,7 @@ async function run() {
     container.registerSingleton("UserService", UserService)
 
     // Import commands
-    await importx(dirname(import.meta.url) + "/commands/*/**/*.{ts,js}")
-
+    await importx(dirname(import.meta.url) + "/{events,commands}/**/**/*.{ts,js}")
     // Let's start the bot
     if (!process.env.BOT_TOKEN) {
         throw Error("Could not find BOT_TOKEN in your environment")
