158dc43c417d4051c7dc65e7a330dd85e9640cb5
Miconen <micropizza.mc@gmail.com>
2025-03-09 15:18:37 +0200

    Remove user service as it's now obsolate

--- src/utils/userUtils/IUserService.ts
+++
@@ -1,6 +0,0 @@
-interface IUserService {
-    getPbs: (userId: string, guildId: string) => Promise<{time: string, boss: string}[]>
-    getAccounts: (userId: string, guildId: string) => Promise<string[]>
-}
-
-export default IUserService
