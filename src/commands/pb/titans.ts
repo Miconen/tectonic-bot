852e7262747c4847977c32bdcf0dac3947d942fe
Miconen <micropizza.mc@gmail.com>
2025-03-09 15:20:03 +0200

    Separate royal titans pbs to their own command

--- src/commands/pb/titans.ts
+++ src/commands/pb/titans.ts
@@ -1,9 +1,13 @@
-import { ApplicationCommandOptionType, CommandInteraction, GuildMember } from "discord.js"
+import { notEmpty } from "@utils/notEmpty.js"
+import {
+    ApplicationCommandOptionType,
+    CommandInteraction,
+    GuildMember,
+} from "discord.js"
 import {
     Discord,
     Guard,
     Slash,
-    SlashChoice,
     SlashGroup,
     SlashOption,
 } from "discordx"
@@ -11,29 +15,18 @@ import IsActivated from "@guards/IsActivated.js"
 import IsAdmin from "@guards/IsAdmin.js"
 import IsValidTime from "@guards/IsValidTime.js"
 import { replyHandler } from "@utils/replyHandler.js"
-import bossCategories from "./func/getBosses.js"
 import submitHandler from "./func/submitHandler.js"
+import getBoss from "./func/getBoss.js"
 
 @Discord()
 @SlashGroup("pb")
 @Guard(IsAdmin, IsValidTime("time"), IsActivated())
-class miscellaneouspb {
-    @Slash({
-        name: "miscellaneous",
-        description: "Request your new pb to be added",
-    })
+class royaltitanspb {
+    @Slash({ name: "titans", description: "Request your new pb to be added" })
     async miscellaneous(
-        @SlashChoice(...bossCategories["Miscellaneous"])
-        @SlashOption({
-            name: "boss",
-            description: "Boss to submit time for",
-            required: true,
-            type: ApplicationCommandOptionType.String,
-        })
-        boss: string,
         @SlashOption({
             name: "time",
-            description: "Miscellaneous boss pb time",
+            description: "Royal Titans pb time",
             required: true,
             type: ApplicationCommandOptionType.String,
         })
@@ -45,12 +38,19 @@ class miscellaneouspb {
             type: ApplicationCommandOptionType.User,
         })
         player: GuildMember,
+        @SlashOption({
+            name: "player2",
+            description: "Teammate discord @name",
+            required: false,
+            type: ApplicationCommandOptionType.User,
+        })
+        player2: GuildMember | null,
         interaction: CommandInteraction,
     ) {
-        let team = [player.user.id]
+        let team = [player.user.id, player2?.user.id].filter(notEmpty)
 
         await interaction.deferReply();
-        let response = await submitHandler(boss, time, team, interaction)
+        let response = await submitHandler(getBoss("royal_titans", team), time, team, interaction)
         await replyHandler(response, interaction);
     }
 }
