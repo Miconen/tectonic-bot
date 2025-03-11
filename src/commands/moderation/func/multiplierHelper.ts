2e7a2c64f50ed9ef834968e507d8dd9c026f067d
Miconen <micropizza.mc@gmail.com>
2025-03-11 22:48:08 +0200

    Fix multiplier helper using old syntax

--- src/commands/moderation/func/multiplierHelper.ts
+++ src/commands/moderation/func/multiplierHelper.ts
@@ -1,14 +1,17 @@
 import { CommandInteraction } from "discord.js"
 import { Requests } from "@requests/main.js"
+import { getString } from "@utils/stringRepo"
 
 const multiplierHelper = async (
     multiplier: number,
     interaction: CommandInteraction
 ) => {
-    let newMultiplier = await Requests.updateGuild(
-        interaction.guild!.id,
-        { type: "multiplier", multiplier }
-    )
+    if (!interaction.guild) {
+        await interaction.reply({ content: getString("errors", "noGuild"), ephemeral: true });
+        return;
+    }
+
+    let newMultiplier = await Requests.updateGuild(interaction.guild.id, { multiplier })
 
     let response = "Something went wrong..."
 
