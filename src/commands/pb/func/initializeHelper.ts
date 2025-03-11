275a27f19675d1b4a069dc3444368c5151dd6e88
Miconen <micropizza.mc@gmail.com>
2025-03-11 17:04:58 +0200

    Fix pb leaderboard initialization

--- src/commands/pb/func/initializeHelper.ts
+++ src/commands/pb/func/initializeHelper.ts
@@ -1,6 +1,6 @@
 import { Requests } from "@requests/main.js"
 import { CategoryUpdate } from "@typings/requests.js"
-import { notEmpty } from "@utils/notEmpty.js"
+import { formatGuildTimes } from "@utils/guilds.js"
 import { getString } from "@utils/stringRepo.js"
 import { CommandInteraction, TextChannel } from "discord.js"
 import embedBuilder from "./embedBuilder.js"
@@ -15,7 +15,6 @@ async function initializeHelper(interaction: CommandInteraction) {
     }
     const guildId = interaction.guild.id
     let channel = interaction.channel as TextChannel
-    //let member = interaction.member as GuildMember
 
     await interaction.deferReply({ ephemeral: true })
 
@@ -23,42 +22,18 @@ async function initializeHelper(interaction: CommandInteraction) {
     const res = await Requests.getGuildTimes(guildId)
 
     if (res.error) {
-        await interaction.reply({ content: getString("errors", "guildTimes", { error: res.message }), ephemeral: true });
+        await interaction.deleteReply();
+        await interaction.followUp({ content: getString("errors", "apiError", { activity: "fetching guild times", error: res.message }), ephemeral: true });
         return;
     }
 
     await interaction.editReply({ content: "Removing old category embeds..." })
-    //await removeOldEmbeds(guildId, interaction.client)
+    await removeOldEmbeds(res.data, interaction.client)
 
     await interaction.editReply({ content: "Creating embeds..." })
 
-    // Create combined bosses data
-    const bosses = res.data.guild_bosses.map(gb => {
-        let pb = res.data.pbs.find(t => t.run_id === gb.pb_id)
-        let teammates = res.data.teammates.filter(tm => tm.run_id === gb.pb_id)
-
-        // Find all guild_bosses entries for this boss
-        const boss = res.data.bosses.find(b => b.name === gb.boss);
-
-        if (!boss) return
-
-        // Return combined data
-        return { ...gb, ...boss, pb, teammates };
-    }).filter(notEmpty);
-
     // Create combined categories data
-    const categories = res.data.guild_categories.map(gc => {
-        let bs = bosses.filter(b => b.category === gc.category)
-
-        // Find all guild_categories entries for this category
-        const category = res.data.categories.find(c => gc.category === c.name);
-
-        if (!category) return
-
-        // Return combined data
-        return { ...gc, ...category, bosses: bs };
-    }).filter(notEmpty).sort((a, b) => a.order - b.order);
-
+    const categories = formatGuildTimes(res.data)
     const players = new Set<string>(res.data.teammates.map(t => t.user_id))
     const members = await interaction.guild.members.fetch({ user: Array.from(players) })
     const msgs: CategoryUpdate[] = []
@@ -71,34 +46,36 @@ async function initializeHelper(interaction: CommandInteraction) {
         let fields: TimeField[] = []
 
         for (let boss of category.bosses) {
-            let formattedTime = "No time yet"
+            let time = "No time yet"
             if (boss.pb) {
-                formattedTime = TimeConverter.ticksToTime(boss.pb.time)
+                time = TimeConverter.ticksToTime(boss.pb.time)
             }
 
-            let formattedTeam = ""
             const team = boss.teammates.map(
                 (player) => `**${members.get(player.user_id)?.displayName}**`
-            )
-            if (team) {
-                formattedTeam = `${team?.join(", ")}`
-            }
+            ).join(", ")
 
             fields.push({
                 name: boss.display_name,
-                value: `${formattedTime} ${formattedTeam}`,
+                value: `${time} ${team}`,
             })
         }
 
-        embed.addFields(...fields)
+        embed.addFields(fields)
 
         let { id } = await channel.send({ embeds: [embed] })
         msgs.push({ message_id: id, category: category.category })
     }
 
     await interaction.editReply({ content: "Storing data..." })
-    await Requests.updateGuild(guildId, { pb_channel: interaction.channelId, category_messages: msgs })
-    await interaction.editReply({ content: "Finished" })
+    const update = await Requests.updateGuild(guildId, { pb_channel: interaction.channelId, category_messages: msgs })
+
+    await interaction.deleteReply();
+    if (update.error) {
+        await interaction.followUp({ content: "Error " })
+        return
+    }
+    await interaction.followUp({ content: "Finished" })
 }
 
 export default initializeHelper
