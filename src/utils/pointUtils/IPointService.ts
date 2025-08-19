b90a2bcf0a28b4060e9d73db7105becd69aa07d0
Miconen <micropizza.mc@gmail.com>
2025-08-20 00:03:00 +0300

    Slim PointService down and simplify interface

--- src/utils/pointUtils/IPointService.ts
+++ src/utils/pointUtils/IPointService.ts
@@ -2,16 +2,10 @@ import type { BaseInteraction, Collection, GuildMember } from "discord.js";
 
 interface IPointService {
 	givePoints: (
-		addedPoints: number,
-		users: GuildMember,
+		addedPoints: number | string,
+		users: GuildMember | Collection<string, GuildMember>,
 		interaction: BaseInteraction,
-	) => Promise<string>;
-	givePointsToMultiple: (
-		addedPoints: number,
-		users: Collection<string, GuildMember>,
-		interaction: BaseInteraction,
-		extraPoints?: { [key: string]: number },
-	) => Promise<string[]>;
+	) => Promise<string | string[]>;
 }
 
 export default IPointService;
--- src/utils/pointUtils/PointService.ts
+++ src/utils/pointUtils/PointService.ts
@@ -1,6 +1,8 @@
 import { Requests } from "@requests/main.js";
+import { getPoints } from "@utils/pointSources.js";
 import { getString } from "@utils/stringRepo.js";
-import type { BaseInteraction, Collection, GuildMember } from "discord.js";
+import type { BaseInteraction, GuildMember } from "discord.js";
+import { Collection } from "discord.js";
 import { inject, injectable, singleton } from "tsyringe";
 import type IRankService from "../rankUtils/IRankService.js";
 import type IPointService from "./IPointService.js";
@@ -10,11 +12,31 @@ import type IPointService from "./IPointService.js";
 export class PointService implements IPointService {
 	constructor(@inject("RankService") private rankService: IRankService) {}
 
-	async givePointsToMultiple(
+	async givePoints(
+		value: string | number,
+		target: GuildMember | Collection<string, GuildMember>,
+		interaction: BaseInteraction,
+	) {
+		if (!interaction.guild) return getString("errors", "noGuild");
+
+		// Ugly ternary to extract points from value parameter
+		const points =
+			typeof value === "number"
+				? value
+				: await getPoints(value, interaction.guild.id);
+
+		// Handle single vs multiple GuildMembers
+		if (target instanceof Collection) {
+			return this.giveToManyHandler(points, target, interaction);
+		}
+
+		return this.giveHandler(points, target, interaction);
+	}
+
+	private async giveToManyHandler(
 		addedPoints: number,
 		targets: Collection<string, GuildMember>,
 		interaction: BaseInteraction,
-		extraPoints?: { [key: string]: number },
 	) {
 		if (!interaction.guild) return [getString("errors", "noGuild")];
 		const response: string[] = [];
@@ -81,7 +103,7 @@ export class PointService implements IPointService {
 		return response;
 	}
 
-	async givePoints(
+	private async giveHandler(
 		addedPoints: number,
 		target: GuildMember,
 		interaction: BaseInteraction,
@@ -93,58 +115,54 @@ export class PointService implements IPointService {
 			points: { type: "custom", amount: addedPoints },
 		});
 
-		if (res.error) {
-			if (res.status === 404) {
-				return getString("accounts", "notActivated", {
-					username: target.displayName,
-				});
-			}
-
-			return getString("errors", "givingPoints");
-		}
-
-		if (!res.data) {
+		if (res.status === 404) {
 			return getString("accounts", "notActivated", {
 				username: target.displayName,
 			});
 		}
 
-		if (res.status === 200) {
-			const newPoints = res.data.points;
-
-			const response: string[] = [];
-			const member = interaction.member as GuildMember;
-
-			response.push(
-				getString("ranks", "pointsGranted", {
-					username: target.displayName,
-					pointsGiven: addedPoints,
-					grantedBy: member.displayName,
-					totalPoints: newPoints,
-				}),
-			);
-			const newRank = await this.rankService.rankUpHandler(
-				interaction,
-				target,
-				newPoints - addedPoints,
-				newPoints,
-			);
-
-			if (!newRank) return response.join("\n");
-
-			// Concatenate level up message to response if user leveled up
-			const newRankIcon = this.rankService.getIcon(newRank);
-			response.push(
-				getString("ranks", "levelUpMessage", {
-					username: target.displayName,
-					icon: newRankIcon,
-					rankName: newRank,
-				}),
-			);
-
-			return response.join("\n");
+		if (res.error) {
+			return getString("errors", "givingPoints");
 		}
 
-		return getString("errors", "givingPoints");
+		if (!res.data) {
+			return getString("errors", "internalError", {
+				username: target.displayName,
+			});
+		}
+
+		const newPoints = res.data.points;
+
+		const response: string[] = [];
+		const member = interaction.member as GuildMember;
+
+		response.push(
+			getString("ranks", "pointsGranted", {
+				username: target.displayName,
+				pointsGiven: addedPoints,
+				grantedBy: member.displayName,
+				totalPoints: newPoints,
+			}),
+		);
+		const newRank = await this.rankService.rankUpHandler(
+			interaction,
+			target,
+			newPoints - addedPoints,
+			newPoints,
+		);
+
+		if (!newRank) return response.join("\n");
+
+		// Concatenate level up message to response if user leveled up
+		const newRankIcon = this.rankService.getIcon(newRank);
+		response.push(
+			getString("ranks", "levelUpMessage", {
+				username: target.displayName,
+				icon: newRankIcon,
+				rankName: newRank,
+			}),
+		);
+
+		return response.join("\n");
 	}
 }
