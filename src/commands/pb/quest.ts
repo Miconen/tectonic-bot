56e6ccbdcc6745f17c6b23665b369c73cb440934
Mico <9155442+Miconen@users.noreply.github.com>
2025-08-23 22:41:28 +0300

    Remove duo bosses and add quest bosses (#106)
    * Remove author from embeds for better visual clarity
    
    * Improve formatting of pb leaderboard entries
    
    * Added logging folder to tsconfig
    
    * Added LoggerGuard to global guards
    
    * Enable silent bot init logging
    
    * Change bot init logging to use Pino
    
    * Implement better logging throughout the application
    
    * Fix events cache not getting hit
    
    * Add yama to boss pbs
    
    * Add rotating discord statuses
    
    * Add barebones version of a event lookup command
    
    * Remove unused import
    
    * Improve logging of time handling
    
    * Fix typings to match API response
    
    * Add event specific strings
    
    * Remove deprecated command
    
    * Narrow interface down to current needs
    
    * Streamline point service to require minimal interface and handle more cases
    
    * Remove unused string
    
    * Add missing parameters to string literal
    
    * Update packages
    
    * Fix options not existing in CommandInteraction anymore
    
    * Add buttons to splits for accepting and denying
    
    * Improve wording on split and achievement requesting
    
    * Remove solo variants from duo bosses
    
    * Add quest bosses

--- src/commands/pb/quest.ts
+++ src/commands/pb/quest.ts
@@ -1,26 +1,40 @@
 import IsActivated from "@guards/IsActivated.js";
 import IsAdmin from "@guards/IsAdmin.js";
 import IsValidTime from "@guards/IsValidTime.js";
-import { notEmpty } from "@utils/notEmpty.js";
 import { replyHandler } from "@utils/replyHandler.js";
 import {
 	ApplicationCommandOptionType,
 	type CommandInteraction,
 	type GuildMember,
 } from "discord.js";
-import { Discord, Guard, Slash, SlashGroup, SlashOption } from "discordx";
-import getBoss from "./func/getBoss.js";
+import {
+	Discord,
+	Guard,
+	Slash,
+	SlashChoice,
+	SlashGroup,
+	SlashOption,
+} from "discordx";
+import bossCategories from "./func/getBosses.js";
 import submitHandler from "./func/submitHandler.js";
 
 @Discord()
 @SlashGroup("pb")
 @Guard(IsAdmin, IsValidTime("time"), IsActivated())
-class royaltitanspb {
-	@Slash({ name: "titans", description: "Request your new pb to be added" })
-	async miscellaneous(
+class quest {
+	@Slash({ name: "quest", description: "Request your new pb to be added" })
+	async quest(
+		@SlashChoice(...bossCategories["Quest Bosses"])
+		@SlashOption({
+			name: "boss",
+			description: "Boss to submit time for",
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		})
+		boss: string,
 		@SlashOption({
 			name: "time",
-			description: "Royal Titans pb time",
+			description: "Quest pb time",
 			required: true,
 			type: ApplicationCommandOptionType.String,
 		})
@@ -32,24 +46,12 @@ class royaltitanspb {
 			type: ApplicationCommandOptionType.User,
 		})
 		player: GuildMember,
-		@SlashOption({
-			name: "player2",
-			description: "Teammate discord @name",
-			required: false,
-			type: ApplicationCommandOptionType.User,
-		})
-		player2: GuildMember,
 		interaction: CommandInteraction,
 	) {
-		const team = [player.user.id, player2?.user.id].filter(notEmpty);
+		const team = [player.user.id];
 
 		await interaction.deferReply();
-		const response = await submitHandler(
-			getBoss("royal_titans", team),
-			time,
-			team,
-			interaction,
-		);
+		const response = await submitHandler(boss, time, team, interaction);
 		await replyHandler(response, interaction);
 	}
 }
