29fb676b26f1370dffc9f5ecf20b80c3c75e7208
Miconen <micropizza.mc@gmail.com>
2025-08-23 22:38:11 +0300

    Add quest bosses

---
+++ src/commands/pb/quest.ts
@@ -0,0 +1,57 @@
+import IsActivated from "@guards/IsActivated.js";
+import IsAdmin from "@guards/IsAdmin.js";
+import IsValidTime from "@guards/IsValidTime.js";
+import { replyHandler } from "@utils/replyHandler.js";
+import {
+	ApplicationCommandOptionType,
+	type CommandInteraction,
+	type GuildMember,
+} from "discord.js";
+import {
+	Discord,
+	Guard,
+	Slash,
+	SlashChoice,
+	SlashGroup,
+	SlashOption,
+} from "discordx";
+import bossCategories from "./func/getBosses.js";
+import submitHandler from "./func/submitHandler.js";
+
+@Discord()
+@SlashGroup("pb")
+@Guard(IsAdmin, IsValidTime("time"), IsActivated())
+class quest {
+	@Slash({ name: "quest", description: "Request your new pb to be added" })
+	async quest(
+		@SlashChoice(...bossCategories["Quest Bosses"])
+		@SlashOption({
+			name: "boss",
+			description: "Boss to submit time for",
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		})
+		boss: string,
+		@SlashOption({
+			name: "time",
+			description: "Quest pb time",
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		})
+		time: string,
+		@SlashOption({
+			name: "player",
+			description: "Player discord @name",
+			required: true,
+			type: ApplicationCommandOptionType.User,
+		})
+		player: GuildMember,
+		interaction: CommandInteraction,
+	) {
+		const team = [player.user.id];
+
+		await interaction.deferReply();
+		const response = await submitHandler(boss, time, team, interaction);
+		await replyHandler(response, interaction);
+	}
+}
