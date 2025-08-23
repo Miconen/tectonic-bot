2ef159744ecacad1d54cd4b91136db56e5296e2c
Miconen <micropizza.mc@gmail.com>
2025-08-23 16:37:19 +0300

    Implement better logging throughout the application

--- src/utils/achievements.ts
+++ src/utils/achievements.ts
@@ -1,19 +1,17 @@
+import { withInitLogging } from "@logging/context";
 import { Requests } from "@requests/main";
 import type { Achievement } from "@typings/requests";
 import { TTLCache } from "@utils/ttlCache";
 
 export const Achievements = new TTLCache<Achievement>(null);
 
-const res = await Requests.getAchievements();
+const res = await withInitLogging(
+	"Achievement initialization",
+	Requests.getAchievements,
+);
 
 if (res.error) {
 	throw new Error("Could not fetch achievement list from database");
-}
-
-if (!res.data.length) {
-	console.log(
-		"Database currently does not include any achievements, consider adding some...",
-	);
 }
 
 for (const achievement of res.data) {
