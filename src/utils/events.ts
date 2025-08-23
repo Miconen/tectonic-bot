9342b4de73dfe67cc940c8c5d286a4f1b6fa3f60
Miconen <micropizza.mc@gmail.com>
2025-08-23 18:08:22 +0300

    Fix events cache not getting hit

---
+++ src/utils/events.ts
@@ -0,0 +1,29 @@
+import { getLogger } from "@logging/context";
+import { Requests } from "@requests/main";
+import type { GuildEvent } from "@typings/requests";
+import { TTLCache } from "@utils/ttlCache";
+
+export const GuildEvents = new TTLCache<GuildEvent[]>();
+
+export async function getEvents(guild_id: string) {
+	await populateEvents(guild_id);
+	return GuildEvents.get(guild_id);
+}
+
+async function populateEvents(guild_id: string) {
+	const logger = getLogger();
+
+	if (GuildEvents.has(guild_id)) return;
+
+	const res = await Requests.getEvents(guild_id);
+	if (res.error) {
+		logger.error({ err: res.message }, "Error fetching guild events");
+		return;
+	}
+
+	if (!res.data) {
+		logger.warn("No guild events found");
+	}
+
+	GuildEvents.set(guild_id, res.data);
+}
