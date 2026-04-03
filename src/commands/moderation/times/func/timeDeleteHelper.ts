033454328c918d80886dbedf9b16fbff7b45a446
Mico <9155442+Miconen@users.noreply.github.com>
2026-04-03 18:16:26 +0300

    Add a `moderation times clear` and rename old removing to reverting (#125)
    Co-authored-by: Miconen <mico.rintala@gmail.com>

---
+++ src/commands/moderation/times/func/timeDeleteHelper.ts
@@ -0,0 +1,49 @@
+import updateEmbed from "@commands/pb/func/updateEmbed";
+import type { ApiResponse } from "@typings/requests";
+import { invalidateGuildCache } from "@utils/guildTimes";
+import { replyHandler } from "@utils/replyHandler";
+import { getString } from "@utils/stringRepo";
+import type { CommandInteraction } from "discord.js";
+
+export async function timeDeleteHelper(
+  boss: string,
+  interaction: CommandInteraction,
+  strategy: (guild_id: string, boss: string) => Promise<ApiResponse<unknown>>,
+  successKey: string
+) {
+  if (!interaction.guild) {
+    return await replyHandler(getString("errors", "noGuild"), interaction, {
+      ephemeral: true,
+    });
+  }
+
+  const res = await strategy(interaction.guild.id, boss);
+  if (res.error && res.code === 1008) {
+    return await replyHandler(getString("times", "timeNotFound"), interaction, {
+      ephemeral: true,
+    });
+  }
+
+  if (res.error) {
+    return await replyHandler(
+      getString("errors", "internalError"),
+      interaction,
+      { ephemeral: true }
+    );
+  }
+
+  invalidateGuildCache(interaction.guild.id);
+
+  await replyHandler(
+    getString("moderation", successKey, { boss }),
+    interaction,
+    { ephemeral: true }
+  );
+
+  const success = await updateEmbed(boss, interaction);
+  if (!success) {
+    await replyHandler(getString("times", "failedUpdatingEmbed"), interaction, {
+      ephemeral: true,
+    });
+  }
+}
--- src/utils/replyHandler.ts
+++ src/utils/replyHandler.ts
@@ -5,62 +5,62 @@ import { CommandInteraction, TextChannel } from "discord.js";
  * Handles the reply to a Discord interaction, deferring or replying based on context, as you cannot reply to deferred commands.
  */
 export async function replyHandler(
-	message: string,
-	interaction: CommandInteraction | ButtonInteraction,
-	options?: { ephemeral?: boolean },
+  message: string,
+  interaction: CommandInteraction | ButtonInteraction,
+  options?: { ephemeral?: boolean }
 ) {
-	const CHARACTER_LIMIT = 2000;
+  const CHARACTER_LIMIT = 2000;
 
-	if (message.length >= CHARACTER_LIMIT) {
-		for (const chunk of splitMessage(message, CHARACTER_LIMIT)) {
-			await replyer(chunk, interaction, true, options);
-		}
+  if (message.length >= CHARACTER_LIMIT) {
+    for (const chunk of splitMessage(message, CHARACTER_LIMIT)) {
+      await replyer(chunk, interaction, true, options);
+    }
 
-		if (interaction instanceof CommandInteraction && interaction.deferred) {
-			await interaction.deleteReply();
-		}
+    if (interaction instanceof CommandInteraction && interaction.deferred) {
+      await interaction.deleteReply();
+    }
 
-		return;
-	}
+    return;
+  }
 
-	return await replyer(message, interaction, false, options);
+  return await replyer(message, interaction, false, options);
 }
 
 async function replyer(
-	message: string,
-	interaction: CommandInteraction | ButtonInteraction,
-	split?: boolean,
-	options?: { ephemeral?: boolean },
+  message: string,
+  interaction: CommandInteraction | ButtonInteraction,
+  split?: boolean,
+  options?: { ephemeral?: boolean }
 ) {
-	const reply = { content: message, ...options };
+  const reply = { content: message, ...options };
 
-	if (interaction.channel instanceof TextChannel && split) {
-		return await interaction.channel.send(reply);
-	}
+  if (interaction.channel instanceof TextChannel && split) {
+    return await interaction.channel.send(reply);
+  }
 
-	if (interaction instanceof CommandInteraction && interaction.deferred) {
-		return await interaction.followUp(reply);
-	}
+  if (interaction instanceof CommandInteraction && interaction.deferred) {
+    return await interaction.followUp(reply);
+  }
 
-	return await interaction.reply(reply);
+  return await interaction.reply(reply);
 }
 
 function splitMessage(message: string, CHARACTER_LIMIT: number) {
-	const chunks = [];
-	let m = message;
+  const chunks = [];
+  let m = message;
 
-	while (m.length > CHARACTER_LIMIT) {
-		let lastLineBreakIndex = m.lastIndexOf("\n", CHARACTER_LIMIT - 1);
-		if (lastLineBreakIndex === -1) {
-			lastLineBreakIndex = CHARACTER_LIMIT;
-		}
+  while (m.length > CHARACTER_LIMIT) {
+    let lastLineBreakIndex = m.lastIndexOf("\n", CHARACTER_LIMIT - 1);
+    if (lastLineBreakIndex === -1) {
+      lastLineBreakIndex = CHARACTER_LIMIT;
+    }
 
-		chunks.push(m.substring(0, lastLineBreakIndex));
-		m = m.substring(lastLineBreakIndex).trim();
-	}
+    chunks.push(m.substring(0, lastLineBreakIndex));
+    m = m.substring(lastLineBreakIndex).trim();
+  }
 
-	// Add the remaining part of the message
-	chunks.push(m);
+  // Add the remaining part of the message
+  chunks.push(m);
 
-	return chunks;
+  return chunks;
 }
