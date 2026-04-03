5d33a35a7dcdb80d1b5a121d8d914778bdca8dd0
Miconen <mico.rintala@gmail.com>
2026-04-03 18:09:57 +0300

    Add a `moderation times clear` and rename old removing to reverting

--- src/commands/moderation/times/times.ts
+++ src/commands/moderation/times/times.ts
@@ -1,86 +1,113 @@
 import {
-	ApplicationCommandOptionType,
-	type GuildMember,
-	type CommandInteraction,
+  ApplicationCommandOptionType,
+  type GuildMember,
+  type CommandInteraction,
 } from "discord.js";
 import { Discord, Guard, Slash, SlashGroup, SlashOption } from "discordx";
 import IsAdmin from "@guards/IsAdmin.js";
 import { bossTimePicker } from "@utils/pickers";
-import { revertTimeHelper } from "./func/removeTimeHelper";
 import { addUserToTimeHelper } from "./func/addUserToTimeHelper";
 import { removeUserFromTimeHelper } from "./func/removeUserFromTimeHelper";
+import { timeDeleteHelper } from "./func/timeDeleteHelper";
+import { Requests } from "@requests/main";
 
 @Discord()
 @SlashGroup({
-	name: "times",
-	description: "Manage times",
-	root: "moderation",
+  name: "times",
+  description: "Manage times",
+  root: "moderation",
 })
 @SlashGroup("times", "moderation")
 @Guard(IsAdmin)
 class Times {
-	@Slash({ name: "revert", description: "Revert a time to an older one" })
-	async remove(
-		@SlashOption({
-			name: "boss",
-			description: "Boss name to remove pb from",
-			required: true,
-			type: ApplicationCommandOptionType.String,
-			autocomplete: bossTimePicker,
-		})
-		boss: string,
-		interaction: CommandInteraction,
-	) {
-		await interaction.deferReply()
-		await revertTimeHelper(boss, interaction);
-	}
+  @Slash({ name: "revert", description: "Revert a time to an older one" })
+  async revert(
+    @SlashOption({
+      name: "boss",
+      description: "Boss name to revert pb of",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+      autocomplete: bossTimePicker,
+    })
+    boss: string,
+    interaction: CommandInteraction
+  ) {
+    await interaction.deferReply();
+    await timeDeleteHelper(
+      boss,
+      interaction,
+      Requests.revertGuildBossPb,
+      "timeRevert"
+    );
+  }
 
-	@Slash({ name: "adduser", description: "Manually add a user to a time" })
-	async adduser(
-		@SlashOption({
-			name: "username",
-			description: "@User tag to add to a time",
-			required: true,
-			type: ApplicationCommandOptionType.User,
-		})
-		user: GuildMember,
-		@SlashOption({
-			name: "boss",
-			description: "Boss name to add user to",
-			required: true,
-			type: ApplicationCommandOptionType.String,
-			autocomplete: bossTimePicker,
-		})
-		boss: string,
-		interaction: CommandInteraction,
-	) {
-		await interaction.deferReply()
-		await addUserToTimeHelper(user, boss, interaction);
-	}
+  @Slash({ name: "clear", description: "Clear a time from a boss" })
+  async clear(
+    @SlashOption({
+      name: "boss",
+      description: "Boss name to remove pb from",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+      autocomplete: bossTimePicker,
+    })
+    boss: string,
+    interaction: CommandInteraction
+  ) {
+    await interaction.deferReply();
+    await timeDeleteHelper(
+      boss,
+      interaction,
+      Requests.clearGuildBossPb,
+      "timeClear"
+    );
+  }
 
-	@Slash({
-		name: "removeuser",
-		description: "Manually remove a user from a time",
-	})
-	async removeuser(
-		@SlashOption({
-			name: "username",
-			description: "@User tag to remove from a time",
-			required: true,
-			type: ApplicationCommandOptionType.User,
-		})
-		user: GuildMember,
-		@SlashOption({
-			name: "boss",
-			description: "Boss name to remove user from",
-			required: true,
-			type: ApplicationCommandOptionType.String,
-			autocomplete: bossTimePicker,
-		})
-		boss: string,
-		interaction: CommandInteraction,
-	) {
-		await interaction.deferReply()
-		await removeUserFromTimeHelper(user, boss, interaction);
-	}
+  @Slash({ name: "adduser", description: "Manually add a user to a time" })
+  async adduser(
+    @SlashOption({
+      name: "username",
+      description: "@User tag to add to a time",
+      required: true,
+      type: ApplicationCommandOptionType.User,
+    })
+    user: GuildMember,
+    @SlashOption({
+      name: "boss",
+      description: "Boss name to add user to",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+      autocomplete: bossTimePicker,
+    })
+    boss: string,
+    interaction: CommandInteraction
+  ) {
+    await interaction.deferReply();
+    await addUserToTimeHelper(user, boss, interaction);
+  }
+
+  @Slash({
+    name: "removeuser",
+    description: "Manually remove a user from a time",
+  })
+  async removeuser(
+    @SlashOption({
+      name: "username",
+      description: "@User tag to remove from a time",
+      required: true,
+      type: ApplicationCommandOptionType.User,
+    })
+    user: GuildMember,
+    @SlashOption({
+      name: "boss",
+      description: "Boss name to remove user from",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+      autocomplete: bossTimePicker,
+    })
+    boss: string,
+    interaction: CommandInteraction
+  ) {
+    await interaction.deferReply();
+    await removeUserFromTimeHelper(user, boss, interaction);
+  }
 }
--- src/requests/guild.ts
+++ src/requests/guild.ts
@@ -1,174 +1,186 @@
 import type { WomParticipations } from "@typings/womRequests";
 import type {
-	Boss,
-	CompetitionResponse,
-	EventWinParam,
-	Guild,
-	GuildPointSource,
-	DetailedGuild,
-	GuildUpdate,
-	NewTime,
-	TimeResponse,
-	User,
-	EventUpdateParam,
-	EventDetails,
+  Boss,
+  CompetitionResponse,
+  EventWinParam,
+  Guild,
+  GuildPointSource,
+  DetailedGuild,
+  GuildUpdate,
+  NewTime,
+  TimeResponse,
+  User,
+  EventUpdateParam,
+  EventDetails,
 } from "@typings/requests";
 import { fetchData } from "./main";
 
 export async function getLeaderboard(guild_id: string) {
-	const url = `guilds/${guild_id}/leaderboard`;
-	const leaderboard = await fetchData<User[]>(url);
+  const url = `guilds/${guild_id}/leaderboard`;
+  const leaderboard = await fetchData<User[]>(url);
 
-	return leaderboard;
+  return leaderboard;
 }
 
 export async function getGuild(guild_id: string) {
-	const url = `guilds/${guild_id}`;
-	const guild = await fetchData<Guild>(url);
+  const url = `guilds/${guild_id}`;
+  const guild = await fetchData<Guild>(url);
 
-	return guild;
+  return guild;
 }
 
 export async function createGuild(guild_id: string) {
-	const url = "guilds";
-	const options = {
-		method: "POST",
-		body: JSON.stringify({ guild_id }),
-	};
-	const status = await fetchData(url, options);
+  const url = "guilds";
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ guild_id }),
+  };
+  const status = await fetchData(url, options);
 
-	return status;
+  return status;
 }
 
 export async function removeGuild(guild_id: string) {
-	const url = `guilds/${guild_id}`;
-	const options = { method: "DELETE" };
+  const url = `guilds/${guild_id}`;
+  const options = { method: "DELETE" };
 
-	const status = await fetchData(url, options);
+  const status = await fetchData(url, options);
 
-	return status;
+  return status;
 }
 
 export async function updateGuild(guild_id: string, query: GuildUpdate) {
-	const url = `guilds/${guild_id}`;
-	const options = {
-		method: "PUT",
-		body: JSON.stringify({
-			multiplier: query.multiplier,
-			pb_channel_id: query.pb_channel,
-			category_messages: query.category_messages,
-		}),
-	};
-	const status = await fetchData(url, options);
+  const url = `guilds/${guild_id}`;
+  const options = {
+    method: "PUT",
+    body: JSON.stringify({
+      multiplier: query.multiplier,
+      pb_channel_id: query.pb_channel,
+      category_messages: query.category_messages,
+    }),
+  };
+  const status = await fetchData(url, options);
 
-	return status;
+  return status;
 }
 
 export async function newTime(guild_id: string, query: NewTime) {
-	const url = `guilds/${guild_id}/times`;
-	const { user_ids, time, boss_name } = query;
-	const options = {
-		method: "POST",
-		body: JSON.stringify({ user_ids, time, boss_name }),
-	};
-	const status = await fetchData<TimeResponse>(url, options);
+  const url = `guilds/${guild_id}/times`;
+  const { user_ids, time, boss_name } = query;
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ user_ids, time, boss_name }),
+  };
+  const status = await fetchData<TimeResponse>(url, options);
 
-	return status;
+  return status;
 }
 
 export async function getBosses() {
-	const url = "bosses";
-	const status = await fetchData<Boss[]>(url);
+  const url = "bosses";
+  const status = await fetchData<Boss[]>(url);
 
-	return status;
+  return status;
 }
 
 export async function eventCompetition(
-	guild_id: string,
-	competition_id: number,
-	cutoff: number,
+  guild_id: string,
+  competition_id: number,
+  cutoff: number
 ) {
-	const url = `guilds/${guild_id}/wom/competition/${competition_id}/cutoff/${cutoff}`;
-	const status = await fetchData<CompetitionResponse>(url);
+  const url = `guilds/${guild_id}/wom/competition/${competition_id}/cutoff/${cutoff}`;
+  const status = await fetchData<CompetitionResponse>(url);
 
-	return status;
+  return status;
 }
 
 export async function getGuildTimes(guild_id: string) {
-	const url = `guilds/${guild_id}/times`;
-	const status = await fetchData<DetailedGuild>(url);
+  const url = `guilds/${guild_id}/times`;
+  const status = await fetchData<DetailedGuild>(url);
 
-	return status;
+  return status;
 }
 
-export async function removeGuildTime(guild_id: string, time_id: string) {
-	const url = `guilds/${guild_id}/times/${time_id}`;
-	const options = {
-		method: "DELETE",
-	};
-	const status = await fetchData(url, options);
+export async function removeTimeById(guild_id: string, time_id: string) {
+  const url = `guilds/${guild_id}/times/id/${time_id}`;
+  const options = { method: "DELETE" };
+  return await fetchData(url, options);
+}
 
-	return status;
+export async function revertGuildBossPb(guild_id: string, boss: string) {
+  const url = `guilds/${guild_id}/times/${boss}/revert`;
+  const options = { method: "DELETE" };
+  return await fetchData(url, options);
+}
+
+export async function clearGuildBossPb(guild_id: string, boss: string) {
+  const url = `guilds/${guild_id}/times/${boss}/clear`;
+  const options = { method: "DELETE" };
+  return await fetchData(url, options);
 }
 
 export async function eventWinners(guild_id: string, params: EventWinParam) {
-	const endpoint = `guilds/${guild_id}/events`;
+  const endpoint = `guilds/${guild_id}/events`;
 
-	const body =
-		params.type === "individual"
-			? { position_cutoff: params.top, event_id: params.competition }
-			: { team_names: params.team_names, event_id: params.competition };
+  const body =
+    params.type === "individual"
+      ? { position_cutoff: params.top, event_id: params.competition }
+      : { team_names: params.team_names, event_id: params.competition };
 
-	const options = {
-		method: "POST",
-		body: JSON.stringify(body),
-	};
+  const options = {
+    method: "POST",
+    body: JSON.stringify(body),
+  };
 
-	return await fetchData<WomParticipations[]>(endpoint, options);
+  return await fetchData<WomParticipations[]>(endpoint, options);
 }
 
 export async function getEvents(guild_id: string) {
-	const endpoint = `guilds/${guild_id}/events`;
+  const endpoint = `guilds/${guild_id}/events`;
 
-	return await fetchData<EventDetails[]>(endpoint);
+  return await fetchData<EventDetails[]>(endpoint);
 }
 
-export async function updateEvent(guild_id: string, event_id: string, params: EventUpdateParam) {
-	const endpoint = `guilds/${guild_id}/events/${event_id}`;
+export async function updateEvent(
+  guild_id: string,
+  event_id: string,
+  params: EventUpdateParam
+) {
+  const endpoint = `guilds/${guild_id}/events/${event_id}`;
 
-	const options = {
-		method: "PUT",
-		body: JSON.stringify(params),
-	};
+  const options = {
+    method: "PUT",
+    body: JSON.stringify(params),
+  };
 
-	return await fetchData<never>(endpoint, options);
+  return await fetchData<never>(endpoint, options);
 }
 
 export async function deleteEvent(guild_id: string, event_id: string) {
-	const endpoint = `guilds/${guild_id}/events/${event_id}`;
+  const endpoint = `guilds/${guild_id}/events/${event_id}`;
 
-	const options = {
-		method: "DELETE",
-	};
+  const options = {
+    method: "DELETE",
+  };
 
-	return await fetchData<never>(endpoint, options);
+  return await fetchData<never>(endpoint, options);
 }
 
 export async function getGuildPointSources(guild_id: string) {
-	const url = `guilds/${guild_id}/points`;
-	const status = await fetchData<GuildPointSource[]>(url);
+  const url = `guilds/${guild_id}/points`;
+  const status = await fetchData<GuildPointSource[]>(url);
 
-	return status;
+  return status;
 }
 
 export async function getEventParticipants(guild_id: string, event_id: string) {
-	const url = `guilds/${guild_id}/events/${event_id}`;
-	const placements = await fetchData<{
-		participations: {
-			user_id: string;
-			placement: number;
-		}[];
-	}>(url);
+  const url = `guilds/${guild_id}/events/${event_id}`;
+  const placements = await fetchData<{
+    participations: {
+      user_id: string;
+      placement: number;
+    }[];
+  }>(url);
 
-	return placements;
+  return placements;
 }
