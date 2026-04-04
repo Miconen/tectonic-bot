769f0fd5db3fed2ce0c31db95f07ea0f25a7548d
Miconen <mico.rintala@gmail.com>
2026-04-04 21:14:29 +0300

    Fix Bosses TTLCache not having null as expiry value

--- src/commands/pb/func/getBosses.ts
+++ src/commands/pb/func/getBosses.ts
@@ -4,49 +4,49 @@ import type { Boss } from "@typings/requests";
 import { TTLCache } from "@utils/ttlCache";
 import type { SlashChoiceType } from "discordx";
 
-export const Bosses = new TTLCache<Boss>();
+export const Bosses = new TTLCache<Boss>(null);
 
 async function bossesAsChoices() {
-	const res = await Requests.getBosses();
-	if (res.error) {
-		throw new Error(
-			"Could not fetch bosses from database in bossesAsChoices()",
-		);
-	}
+  const res = await Requests.getBosses();
+  if (res.error) {
+    throw new Error(
+      "Could not fetch bosses from database in bossesAsChoices()"
+    );
+  }
 
-	const bossesByCategory: Record<string, SlashChoiceType[]> = {};
+  const bossesByCategory: Record<string, SlashChoiceType[]> = {};
 
-	for (const boss of res.data) {
-		const { category, name, display_name } = boss;
+  for (const boss of res.data) {
+    const { category, name, display_name } = boss;
 
-		Bosses.set(name, boss);
+    Bosses.set(name, boss);
 
-		if (!bossesByCategory[category]) {
-			bossesByCategory[category] = [];
-		}
+    if (!bossesByCategory[category]) {
+      bossesByCategory[category] = [];
+    }
 
-		const choice = {
-			name: display_name,
-			value: name,
-		};
+    const choice = {
+      name: display_name,
+      value: name,
+    };
 
-		// HACK: We want to exclude these from the miscellaneous command since they have their own... for now...
-		if (category === "Miscellaneous" && name === "royal_titans_2") {
-			continue;
-		}
-		if (category === "Miscellaneous" && name === "yama_2") {
-			continue;
-		}
+    // HACK: We want to exclude these from the miscellaneous command since they have their own... for now...
+    if (category === "Miscellaneous" && name === "royal_titans_2") {
+      continue;
+    }
+    if (category === "Miscellaneous" && name === "yama_2") {
+      continue;
+    }
 
-		bossesByCategory[category].push(choice);
-	}
+    bossesByCategory[category].push(choice);
+  }
 
-	return bossesByCategory;
+  return bossesByCategory;
 }
 
 const bossCategories: Record<string, SlashChoiceType[]> = await withInitLogging(
-	"Boss categories initialization",
-	bossesAsChoices,
+  "Boss categories initialization",
+  bossesAsChoices
 );
 
 export default bossCategories;
