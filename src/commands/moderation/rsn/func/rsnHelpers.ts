ae3b6b517512376b3e7df7135af1dda24f0a15d4
Miconen <mico.rintala@gmail.com>
2026-08-08 01:54:06 +0300

    feat: implement api error resolver to rsns

--- src/commands/moderation/rsn/func/rsnHelpers.ts
+++ src/commands/moderation/rsn/func/rsnHelpers.ts
@@ -1,4 +1,5 @@
 import { Requests } from "@requests/main.js";
+import { replyApiError } from "@utils/replyApiError";
 import { replyHandler } from "@utils/replyHandler.js";
 import { getString } from "@utils/stringRepo";
 import type { CommandInteraction, GuildMember } from "discord.js";
@@ -8,28 +9,15 @@ export async function addRsnHelper(
 	rsn: string,
 	interaction: CommandInteraction<"cached">,
 ) {
-	await interaction.deferReply();
-
 	const res = await Requests.addRsn(interaction.guild.id, user.id, rsn);
-	let error = getString("errors", "rsnFail", { rsn });
-
-	if (res.status === 404) {
-		error += `\n${getString("errors", "notActivated", {
-			username: user.displayName,
-		})}`;
-		return await replyHandler(error, interaction);
-	}
-
-	if (res.status === 409) {
-		error += `\n${getString("errors", "rsnExists", {
-			username: user.displayName,
-		})} `;
-		return await replyHandler(error, interaction);
-	}
-
 	if (res.error) {
-		error += `\n${getString("errors", "internalError")}`;
-		return await replyHandler(error, interaction);
+		return await replyApiError(res, interaction, {
+			category: "accountErrors",
+			args: {
+				rsn,
+				username: user.displayName,
+			},
+		});
 	}
 
 	const rsns = await Requests.getUser(interaction.guild.id, {
@@ -66,23 +54,15 @@ export async function removeRsnHelper(
 ) {
 	await interaction.deferReply();
 
-	const removed = await Requests.removeRsn(interaction.guild.id, user.id, rsn);
-
-	if (removed.status === 404) {
-		return await replyHandler(
-			getString("errors", "rsnDoesntExist", {
+	const res = await Requests.removeRsn(interaction.guild.id, user.id, rsn);
+	if (res.error) {
+		return await replyApiError(res, interaction, {
+			category: "accountErrors",
+			args: {
 				rsn,
 				username: user.displayName,
-			}),
-			interaction,
-		);
-	}
-
-	if (removed.error) {
-		return await replyHandler(
-			getString("errors", "internalError"),
-			interaction,
-		);
+			},
+		});
 	}
 
 	return await replyHandler(
