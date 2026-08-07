0c8762ba93d0e24761598e4e158596019246e2da
Miconen <mico.rintala@gmail.com>
2026-08-08 02:06:43 +0300

    feat: added additional user helping information to error messages and made epehemeral by default

--- src/utils/replyApiError.ts
+++ src/utils/replyApiError.ts
@@ -1,10 +1,12 @@
+import { getContext } from "@logging/context";
 import type { ErrorResponse } from "@typings/api/errors";
 import { getApiErrorMessage } from "@utils/errors/api/resolver";
 import { replyHandler } from "@utils/replyHandler";
-import type {
-	ButtonInteraction,
-	CommandInteraction,
-	InteractionReplyOptions,
+import {
+	MessageFlags,
+	type ButtonInteraction,
+	type CommandInteraction,
+	type InteractionReplyOptions,
 } from "discord.js";
 
 type ErrorContext = {
@@ -18,7 +20,9 @@ export function replyApiError(
 	error: ErrorResponse,
 	interaction: CommandInteraction | ButtonInteraction,
 	context?: ErrorContext,
-	options?: ReplyOptions,
+	options: ReplyOptions = { flags: MessageFlags.Ephemeral },
 ) {
-	return replyHandler(getApiErrorMessage(error, context), interaction, options);
+	const store = getContext();
+	const message = `${getApiErrorMessage(error, context)}\n\n-# Give this to Comfy: ${store?.correlationId}`;
+	return replyHandler(message, interaction, options);
 }
