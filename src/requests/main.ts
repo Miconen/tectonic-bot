31bfc12361a7e5bcd33adc2b5cb408846079a7f6
Miconen <mico.rintala@gmail.com>
2026-08-18 14:43:24 +0300

    fix: set default api protocol to be https

--- src/requests/main.ts
+++ src/requests/main.ts
@@ -18,8 +18,9 @@ import type {
 } from "@typings/api/errors";
 import { HTTPError } from "discord.js";
 
+const API_PROTOCOL = process.env.API_PROTOCOL ?? "https";
 const API_URL = process.env.API_URL
-	? `${process.env.API_PROTOCOL}://${process.env.API_URL}/api/v1/`
+	? `${API_PROTOCOL}://${process.env.API_URL}/api/v1/`
 	: "http://localhost:8080/api/v1/";
 const AUTH_KEY = process.env.AUTH_KEY ?? "";
 
